2dc4b4b9b4791c172e0cd1a0740c4b2806b174f3
Deyvid Ordoñez <37844597+deyvid1234@users.noreply.github.com>
2023-10-05 11:55:39 -0600

    Filtro 1 presentador reporte

--- Vorwerk_project/Vorwerk Comission aux Suitelet.js
+++ Vorwerk_project/Vorwerk Comission aux Suitelet.js
@@ -222,11 +222,11 @@ function(file,search,plugin,runtime,task) {
                     operator: 'anyof',
                     values: [1,2,3,4,5,6,7,8]
                 },
-                /*{
+                {
                   name: 'internalid',
-                  operator: 'is',
-                  values: [3346123]
-               },*/
+                  operator: 'anyof',
+                  values: [21613,77906,197346]
+               },
                 
               ],
               columns: [
