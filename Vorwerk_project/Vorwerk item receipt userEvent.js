e47140a12d9ce46dc90781a406df36fe3d82d009
GriselOB <132954030+GriselOB@users.noreply.github.com>
2023-10-21 00:36:35 -0600

    Fix item receipt

---
+++ Vorwerk_project/Vorwerk item receipt userEvent.js
@@ -0,0 +1,73 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define(['N/runtime','N/url','N/https'],
+
+function(runtime,url,https) {
+   
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {string} scriptContext.type - Trigger type
+     * @param {Form} scriptContext.form - Current form
+     * @Since 2015.2
+     */
+    function beforeLoad(scriptContext) {
+    	try{
+    		var record = scriptContext.newRecord;
+    		var form = scriptContext.
+    		form;
+    		form.clientScriptFileId = (runtime.envType != 'PRODUCTION') ? '1585973' : '1585973';
+       	 	form.addButton({
+                id: 'custpage_btn_item_receipt',
+                label: 'Imprimir Recibo',
+                functionName: 'printReceipt(\''+record.id+'\');'
+            });
+    	}catch(err){
+    		log.error('Errro beforeLoad',err);
+    	}
+    	
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function beforeSubmit(scriptContext) {
+
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function afterSubmit(scriptContext) {
+    	try{
+    		
+    	}catch(err){
+    		log.error("error after submit",err);
+    	}
+    }
+
+  
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    };
+    
+});
