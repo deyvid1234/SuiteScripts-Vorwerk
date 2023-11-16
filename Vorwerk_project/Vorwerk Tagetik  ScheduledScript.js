cc71c72e634819ab5c2120ddaef7d89213b95396
GriselOB <132954030+GriselOB@users.noreply.github.com>
2023-11-16 08:20:30 -0600

    Update Vorwerk Tagetik  ScheduledScript.js
    SKU Spark

--- Vorwerk_project/Vorwerk Tagetik  ScheduledScript.js
+++ Vorwerk_project/Vorwerk Tagetik  ScheduledScript.js
@@ -211,6 +211,7 @@ function(record,search,https,file,http,format,encode,email,runtime,format) {
                     data[i]['item'] == '62221' || 
                     data[i]['item'] == '62431' || 
                     data[i]['item'] == '62752' ||
+                    data[i]['item'] == '62959' ||
                     data[i]['item'] == 'TM6R'     ){
                     isTM = true
                 }
