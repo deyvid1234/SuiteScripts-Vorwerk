0129f3ff58365ab02b6b48837ee7277b2822d9b7
GriselOB <132954030+GriselOB@users.noreply.github.com>
2023-11-23 13:22:31 -0600

    Update Vorwerk Apartado Inventario UE.js

--- Vorwerk_project/Apartado de Inventario/Vorwerk Apartado Inventario UE.js
+++ Vorwerk_project/Apartado de Inventario/Vorwerk Apartado Inventario UE.js
@@ -17,13 +17,13 @@ function(record,search,http,https,encode,runtime,serverWidget) {
      * @Since 2015.2
      */
     function beforeLoad(scriptContext) {
-    	try{
-    		
+        try{
+            
            
-    	}catch(err){
-    		log.error('errorbeforeload',err);
-    	}
-    	
+        }catch(err){
+            log.error('errorbeforeload',err);
+        }
+        
     }
 
     /**
@@ -49,47 +49,156 @@ function(record,search,http,https,encode,runtime,serverWidget) {
      * @Since 2015.2
      */
     function afterSubmit(scriptContext) {
-    	try{
-    		var thisRecord = scriptContext.newRecord;
-        	var oldrecord = scriptContext.oldRecord;
+        try{
+            var thisRecord = scriptContext.newRecord;
+            var oldrecord = scriptContext.oldRecord;
 
-        	
-	    		if( scriptContext.type == 'create' ){ 
-	    			var id_Item = thisRecord.getValue('custrecord_item_apartado')
-	    			var dataItem = search.lookupFields({// Busqueda de Invdentory Item
-		                type: 'item',
-		                id: id_Item,
-		                columns: ['custitem_disponible_eshop','recordtype']//Stock disponible en el campo para eshop, tipo de registro
-		            });
+            
+                if( scriptContext.type == 'create' ){ 
+                    var id_Item = thisRecord.getValue('custrecord_item_apartado')
+                    var dataItem = search.lookupFields({// Busqueda de Invdentory Item
+                        type: 'item',
+                        id: id_Item,
+                        columns: ['custitem_disponible_eshop','recordtype','custitem_transaccion_apartados']//Stock disponible en el campo para eshop, tipo de registro
+                    });
 
-	    			log.debug('dataItem',dataItem)
-	    			var disponible_eshop = parseInt(dataItem['custitem_disponible_eshop']) //Stock dedicado a eshop
-	    			var nuevoApartado = thisRecord.getValue('custrecord_cantidad_apartada')
-		            var itemType = dataItem['recordtype']//Tipo de registro del inventory item
-		            if(disponible_eshop >= 0) {
-		            	log.debug('stocks','disponible_eshop '+disponible_eshop+' nuevoApartado '+nuevoApartado)
-		            	record.submitFields({
-		                type: itemType,
-		                id: id_Item,
-		                values: { custitem_disponible_eshop: nuevoApartado+ disponible_eshop}
-		            	})
+                    log.debug('dataItem',dataItem)
+                    var disponible_eshop = parseInt(dataItem['custitem_disponible_eshop']) //Stock dedicado a eshop
+                    var nuevoApartado = thisRecord.getValue('custrecord_cantidad_apartada')
+                    var location = thisRecord.getValue('custrecord_from_location')
+                    var transaccionApartados= dataItem['custitem_transaccion_apartados']
+                    var name = 3281861
+                    log.debug('transaccionApartados', transaccionApartados.value)
+                    log.debug('transaccionApartados', transaccionApartados.text)
+                    log.debug('location', location)
+                    var itemType = dataItem['recordtype']//Tipo de registro del inventory item
+                    if(disponible_eshop >= 0) {
+                        log.debug('stocks','disponible_eshop '+disponible_eshop+' nuevoApartado '+nuevoApartado)
+                        record.submitFields({
+                        type: itemType,
+                        id: id_Item,
+                        values: { custitem_disponible_eshop: nuevoApartado+ disponible_eshop}
+                        })
 
-		            }
-	    			else {
+                    }
+                    else {
                         record.submitFields({
                         type: itemType,
                         id: id_Item,
                         values: { custitem_disponible_eshop: nuevoApartado}
                         })
                     }
-	    		}
-    		return true
-    		
+                    if(transaccionApartados[0].value == ""){
+                       
+                        log.debug("crear")
+                        var obj_SO= record.create({
+                            type: 'salesorder',
+                            isDynamic: false,
+                        });
+                        obj_SO.setValue({
+                            fieldId: 'customform',
+                            value: 68
+                        });
+                        obj_SO.setValue({
+                            fieldId: 'entity',
+                            value: name
+                        });
+                                               
+                        obj_SO.setSublistValue({
+                            sublistId : 'item',
+                            fieldId : 'location',
+                            line : 0,
+                            value : location
+                        });
+                        obj_SO.setSublistValue({
+                            sublistId : 'item',
+                            fieldId : 'item',
+                            line : 0,
+                            value : id_Item
+                        });
+                        obj_SO.setSublistValue({
+                            sublistId : 'item',
+                            fieldId : 'quantity',
+                            line : 0,
+                            value : nuevoApartado
+                        });
+                        obj_SO.setSublistValue({
+                            sublistId : 'item',
+                            fieldId : 'amount',
+                            line : 0,
+                            value : 0.01
+                        });
+                        
+                        var id_SO = obj_SO.save({
+                            enableSourcing: true,
+                            ignoreMandatoryFields: true
+                        });
+                        log.debug('id_SO', id_SO)
+                        
+
+                        record.submitFields({
+                        type: itemType,
+                        id: id_Item,
+                        values: { custitem_transaccion_apartados: id_SO}
+                        })
+
+                        return id_SO;
+                    }else{
+                         log.debug("cargar")
+                         log.debug('transaccionApartados', transaccionApartados[0].value)
+                        var idSOaCargar = transaccionApartados[0].value
+                        var cargarSO = record.load({
+                            type: record.Type.SALES_ORDER,
+                            id: idSOaCargar,
+                            isDynamic:
+                             false,
+                        });
+                        
+                        var itemLines = cargarSO.getLineCount({
+                            sublistId  : 'item'
+                        });
+                      
+                        for(var i=0; i < itemLines; i++){
+                            var itemId = cargarSO.getSublistValue({
+                                sublistId : 'item',
+                                fieldId   : 'item',
+                                line      : i
+                                });
+                            log.debug('itemId', itemId)
+                            if(id_Item == itemId ){
+                            log.debug("actualizar")
+
+                            var itemQuantity = cargarSO.getSublistValue({
+                                sublistId : 'item',
+                                fieldId   : 'quantity',
+                                line      : i
+                                });  
+                                log.debug('itemQuantity', itemQuantity) 
+
+                            var apartadoTotal= itemQuantity+nuevoApartado
+                            cargarSO.setSublistValue({
+                                sublistId : 'item',
+                                fieldId   : 'quantity',
+                                value: apartadoTotal,
+                                line      : i
+                                });  
+                                
+                                log.debug('apartadoTotal', apartadoTotal)
+                                log.debug('nuevoApartado', nuevoApartado)
+                            }
+                         }
+
+                            
+                        
+                    }
+                }
+            return true
+            
     
-    	}catch(e){
-    		log.debug('Error en afterSubmit',e)
-    	}
-	}
+        }catch(e){
+            log.debug('Error en afterSubmit',e)
+        }
+    }
     
     
     
