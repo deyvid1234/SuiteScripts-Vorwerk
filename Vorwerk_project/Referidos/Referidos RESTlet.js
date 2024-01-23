353a810b7dd908248dc6063bf78f19a8563d2a47
GriselOB <132954030+GriselOB@users.noreply.github.com>
2024-01-23 09:33:50 -0600

    Update Referidos RESTlet.js

--- Vorwerk_project/Referidos/Referidos RESTlet.js
+++ Vorwerk_project/Referidos/Referidos RESTlet.js
@@ -382,7 +382,7 @@ function(record,search,https,file,http,format,encode,email,runtime) {
                                 });
                                 cliente_record.setValue({
                                     fieldId: 'custentityidu_presentador',
-                                    value: req_info.IDUsalesRepActual
+                                    value: req_info.IDUsalesRepNuevo
                                 });
 
                                 salesrepNuevoResponse = req_info.salesrepNuevo
