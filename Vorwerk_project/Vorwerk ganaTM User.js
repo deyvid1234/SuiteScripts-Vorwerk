9ef22b772219a19dd2496177743ecbe594d38016
GriselOB <132954030+GriselOB@users.noreply.github.com>
2024-10-25 15:53:45 -0600

    Update Vorwerk ganaTM User.js

--- Vorwerk_project/Vorwerk ganaTM User.js
+++ Vorwerk_project/Vorwerk ganaTM User.js
@@ -47,58 +47,62 @@ function(record,search,Utils,Dictionary) {
             log.debug("start afeter submit","start");
             
             var rec = scriptContext.newRecord;
-            var rec_type = rec.type;
-            var subtotal = rec.getValue('custrecord_total_reporte_gtm');
-            var total = 0; 
-            var retencion = 0;
-            var sumBonos = Utils.getBonos(2,rec);//el numero indica el tipo de promocion del presentador
-            log.debug('sumBonos',sumBonos);
-            log.debug('subtotal',subtotal);
-            
-                subtotal= subtotal+sumBonos;
-                var rec_related = rec.getValue('custrecord_sub_registro_compensaciones_g');
-                var tmp_period = search.lookupFields({
-                    type: 'customrecord_registro_compensaciones',
-                    id: rec_related,
-                    columns: 'custrecord_periodo_comision'
-                });
+            var registro_congelado = rec.getValue('custrecord_record_frozen_gtm');
+            if(!registro_congelado){
+                var rec_type = rec.type;
+                var subtotal = rec.getValue('custrecord_total_reporte_gtm');
+                var total = 0; 
+                var retencion = 0;
+                var sumBonos = Utils.getBonos(2,rec);//el numero indica el tipo de promocion del presentador
+                log.debug('sumBonos',sumBonos);
+                log.debug('subtotal',subtotal);
                 
-                var period = tmp_period.custrecord_periodo_comision[0].value;
-                var listISR = Utils.getISRData(period);//se extrae la lista de isr 
-                var isr = listISR.isrList;
-                for(var x in isr){
-                    if(subtotal > parseFloat(isr[x].inferiorLimit)  && subtotal <= parseFloat(isr[x].topLimit)){//se valida que el subtotal se mayor al limite inferior y menor que el limite superior
-                        
-                        var ret_aux = subtotal -parseFloat(isr[x].inferiorLimit)//subtotal menos limite inferior
-                        log.debug('ret_aux',ret_aux);
-                        var porcentaje = parseFloat(isr[x].percentOverIL)/100;//se obtiene el porcentaje de cuota
-                        log.debug('porcentaje',porcentaje);
-                        var base = ret_aux*porcentaje //se extrae la base de isr
-                        log.debug('base',base);
-                        retencion = base+parseFloat(isr[x].quota);//se obtiene la retencion
-                        
-                        log.debug('retencion',retencion);
-                        break;
-                    }
-                }
-                total = subtotal-retencion;
-                try{
-                    var rec_jdg = record.load({
-                        type: rec_type,
-                        id: rec.id,
-                        isDynamic: true
+                    subtotal= subtotal+sumBonos;
+                    var rec_related = rec.getValue('custrecord_sub_registro_compensaciones_g');
+                    var tmp_period = search.lookupFields({
+                        type: 'customrecord_registro_compensaciones',
+                        id: rec_related,
+                        columns: 'custrecord_periodo_comision'
                     });
-                    rec_jdg.setValue('custrecord_c_gtm_retencion',retencion);
-                    rec_jdg.setValue('custrecord_c_gtm_total',total);
-                    rec_jdg.setValue('custrecord_c_gtm_subtotal',subtotal);
-                    rec_jdg.save({enableSourcing: false, ignoreMandatoryFields: true });
-                }catch(err_set){
-                    log.error("error set retencion",err_set);
-                }
-                
-                
-                log.debug('listISR',listISR);
-                
+                    
+                    var period = tmp_period.custrecord_periodo_comision[0].value;
+                    var listISR = Utils.getISRData(period);//se extrae la lista de isr 
+                    var isr = listISR.isrList;
+                    for(var x in isr){
+                        if(subtotal > parseFloat(isr[x].inferiorLimit)  && subtotal <= parseFloat(isr[x].topLimit)){//se valida que el subtotal se mayor al limite inferior y menor que el limite superior
+                            
+                            var ret_aux = subtotal -parseFloat(isr[x].inferiorLimit)//subtotal menos limite inferior
+                            log.debug('ret_aux',ret_aux);
+                            var porcentaje = parseFloat(isr[x].percentOverIL)/100;//se obtiene el porcentaje de cuota
+                            log.debug('porcentaje',porcentaje);
+                            var base = ret_aux*porcentaje //se extrae la base de isr
+                            log.debug('base',base);
+                            retencion = base+parseFloat(isr[x].quota);//se obtiene la retencion
+                            
+                            log.debug('retencion',retencion);
+                            break;
+                        }
+                    }
+                    total = subtotal-retencion;
+                    try{
+                        var rec_jdg = record.load({
+                            type: rec_type,
+                            id: rec.id,
+                            isDynamic: true
+                        });
+                        rec_jdg.setValue('custrecord_c_gtm_retencion',retencion);
+                        rec_jdg.setValue('custrecord_c_gtm_total',total);
+                        rec_jdg.setValue('custrecord_c_gtm_subtotal',subtotal);
+                        rec_jdg.save({enableSourcing: false, ignoreMandatoryFields: true });
+                    }catch(err_set){
+                        log.error("error set retencion",err_set);
+                    }
+                    
+                    
+                    log.debug('listISR',listISR);
+                    
+            }
+            
             
             
         }catch(err){
