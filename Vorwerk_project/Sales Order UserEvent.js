d7034b8ebee41bd0ecb63baf841f9be5861c3d02
GriselOB <132954030+GriselOB@users.noreply.github.com>
2025-07-04 13:58:47 -0600

    Update Sales Order UserEvent.js

--- Vorwerk_project/Sales Order UserEvent.js
+++ Vorwerk_project/Sales Order UserEvent.js
@@ -1207,54 +1207,56 @@ function(runtime,config,record,render,runtime,email,search,format,http,https,ser
                 soSearch.run().each(function(r){
                     var internalId = r.getValue('internalid')
                     var tipoVenta = r.getValue('custbody_tipo_venta')
+                    var comStatus = r.getValue('custbody_vw_comission_status')
                     fechaSO = r.getValue('trandate')
                     
                     // Solo agregar la orden al arreglo si no hemos excedido el límite
-                    if (cont <= limit) {
+                    if (cont <= limit && comStatus != 2) {
                         ordenesProcesadas.push(internalId);
+                        cont++
                     }
                     
                    log.debug('[' + salesrep + '] Procesando orden ' + cont + ' de ' + limit + ' - ID: ' + internalId + ' - Tipo: ' + tipoVenta)
                     switch(cont){
-                    case 1:
-                    log.debug('[' + salesrep + '] case1 ',fechaSO)
-                        newStatus = 2
-                        break;
-                    case 2:
-                    log.debug('[' + salesrep + '] case2 ',fechaSO) 
-                        newStatus = 2
-                        break;
-                    case 3: 
-                    log.debug('[' + salesrep + '] case3 ',fechaSO)
-                        if(limit == 3){
-                            newStatus = 3
-                            setfechafin = true 
-                            soGanadora = internalId
-                            log.debug('[' + salesrep + '] *** TM7 GANADA EN ORDEN 3 ***');
-                        }
+                        case 1:
+                        log.debug('[' + salesrep + '] case1 ',fechaSO)
+                            newStatus = 2
+                            break;
+                        case 2:
+                        log.debug('[' + salesrep + '] case2 ',fechaSO) 
+                            newStatus = 2
+                            break;
+                        case 3: 
+                        log.debug('[' + salesrep + '] case3 ',fechaSO)
+                            if(limit == 3){
+                                newStatus = 3
+                                setfechafin = true 
+                                soGanadora = internalId
+                                log.debug('[' + salesrep + '] *** TM7 GANADA EN ORDEN 3 ***');
+                            }
+                            
+                            break;
+                        case 4: 
+                        log.debug('[' + salesrep + '] case4 ',fechaSO)
+                            if(limit == 4){
+                                newStatus = 3
+                                setfechafin = true 
+                                soGanadora = internalId
+                                log.debug('[' + salesrep + '] *** TM7 GANADA EN ORDEN 4 ***');
+                            }
+                            break;
                         
-                        break;
-                    case 4: 
-                    log.debug('[' + salesrep + '] case4 ',fechaSO)
-                        if(limit == 4){
-                            newStatus = 3
-                            setfechafin = true 
-                            soGanadora = internalId
-                            log.debug('[' + salesrep + '] *** TM7 GANADA EN ORDEN 4 ***');
-                        }
-                        break;
+                        default: 
+                            newStatus = ''
+                            log.error('[' + salesrep + '] ERROR switch - contador fuera de rango')
+                            break;
                     
-                    default: 
-                        newStatus = ''
-                        log.error('[' + salesrep + '] ERROR switch - contador fuera de rango')
-                        break;
-                    
-                   }
-                   log.debug('[' + salesrep + '] newStatus ',newStatus)
+                    }
+                    log.debug('[' + salesrep + '] newStatus ',newStatus)
                     log.debug('[' + salesrep + '] fechaSO ',fechaSO)
                     log.debug('[' + salesrep + '] soGanadora ',soGanadora)
                     log.debug('[' + salesrep + '] setfechafin ',setfechafin)
-                    cont++
+                    
                    
                    // Terminar la búsqueda si alcanzamos el límite
                    if(cont > limit){
