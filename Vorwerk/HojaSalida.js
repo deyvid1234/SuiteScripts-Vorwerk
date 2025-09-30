b854bb9486eb932adf7adf051c85e680405f3b3c
GriselOB <132954030+GriselOB@users.noreply.github.com>
2025-09-30 10:24:28 -0600

    Update HojaSalida.js

--- Vorwerk/HojaSalida.js
+++ Vorwerk/HojaSalida.js
@@ -201,7 +201,7 @@ try {
 	for(var i=1;i<itemcount + 1;i++){
 				var tmp_item = hsRec.getLineItemValue('item', 'item', i);
 				nlapiLogExecution('debug', 'tmp_item', tmp_item);
-				if(tmp_item == 2001 || tmp_item == 2170 || tmp_item == 2490|| tmp_item == 2280 || tmp_item == 2571 || tmp_item == 2671){
+				if(tmp_item == 2001 || tmp_item == 2170 || tmp_item == 2490|| tmp_item == 2280 || tmp_item == 2571 || tmp_item == 2671 || tmp_item == 2763){
 					 var subrecord = hsRec.viewLineItemSubrecord('item', 'inventorydetail',i);
 					 nlapiLogExecution('debug', 'subrecord', JSON.stringify(subrecord));
 					 if(subrecord != null) {
